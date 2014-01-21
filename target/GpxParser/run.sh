8f882937a467d6e6d3dc1c2942c8e2b978be9f06
Santiago Higuera <shiguera@mercatorlab.com>
2014-01-21 17:27:02 +0100

    Beginning V3.2.1

--- target/GpxParser/run.sh
+++ target/GpxParser/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
  
-java -cp /home/shiguera/Documents/tesis/workspace/GpxParser/target/GpxParser/lib -jar /home/shiguera/Documents/tesis/workspace/GpxParser/target/GpxParser/GpxParser-3.2-SNAPSHOT.jar $*
+java -cp /home/shiguera/Documents/tesis/workspace/GpxParser/target/GpxParser/lib -jar /home/shiguera/Documents/tesis/workspace/GpxParser/target/GpxParser/GpxParser-3.2.jar $*
